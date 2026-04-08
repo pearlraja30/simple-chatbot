509c1ae666f7538099b6f0038c1c674a26aa867b
pearlraja30 <pearlraja@gmail.com>
2026-04-08 05:41:23 +0530

    api key env change

--- api/chat.py
+++ api/chat.py
@@ -15,7 +15,7 @@ load_dotenv()
 app = FastAPI()
 
 # Initialize Groq client
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+GROQ_API_KEY = os.environ["GROQ_API_KEY"]
 client = Groq(api_key=GROQ_API_KEY)
 
 # Data Store (Global state)
